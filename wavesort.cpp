a536b5dff37e5a6d307fb6aac11a03af69d682f6
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2022-01-18 08:32:55 +0530

    Add files via upload

---
+++ wavesort.cpp
@@ -0,0 +1,30 @@
+/*********************CHANDRAKESH RAM***************************/
+/*=======================PROBLEM STATEMENT==============*/
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int arr[],int i,int j  ){
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+void waveSort(int arr[],int n){
+    for(int i=1;i<n;i+=2){
+        if(arr[i]>arr[i-1]){
+            swap(arr,i,i-1);
+        }
+        if(arr[i]>arr[i+1]  && i<=n-2){
+            swap(arr,i,i+1);
+        }
+    }
+}
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    waveSort(arr,10);
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+/*===========================OUTPUT=====================*/
